5883ff1f74de2360004cb529144721a69a47d44a
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-31 16:58:29 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -33,7 +33,7 @@ class Student
   end
 
   def self.create(name:, grade:)
-    (name, grade).each do |key, value|
+    {:name, :grade}.each do |key, value|
       self.send(("#{key}="), value)
     end
   end

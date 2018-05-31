c65922a89909737849db5fdc25ca41aec7ba7e9e
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-31 16:31:50 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -29,6 +29,6 @@ class Student
 
   def save
     binding.pry
-    DB[:conn].execute("INSERT INTO students (name, grade) VALUES (#{self.name}, #{self.grade})")
+    DB[:conn].execute("INSERT INTO students (name, grade) VALUES (#{self.name}.to_i, #{self.grade})")
   end
 end

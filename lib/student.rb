72918806f91bb3e1c14c8958085df78785b8605d
Temitope Akintoye <ruth_olukunle@yahoo.com>
2018-05-31 17:19:01 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -32,9 +32,7 @@ class Student
     DB[:conn].execute("INSERT INTO students (name, grade) VALUES (#{self.name}, #{self.grade})")
   end
 
-  def self.create(name:, grade:)
-  #  binding.pry
-    @attrubutes = {name: name, grade: grade}
+  def self.create(attributes)
     @attributes.each do |key, value|
       self.send(("#{key}="), value)
     end
